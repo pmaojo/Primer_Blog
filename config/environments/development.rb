d0abaada6401fe55f7fd3031ed617900887c792d
Gerardo Barzola <gbarzolas@pucp.pe>
2016-01-21 18:31:29 +0000

    Instalacion de gema Devise, creacion de vistas y modelo User

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+  
+  config.action_mailer.default_url_options = { host: 'https://blog-grupo-ror-es-gbarzola1.c9users.io/', port: 3000 }
+  
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
