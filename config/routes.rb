27bba35bb8ec6f79fb7de52c8d91a3a0a6be3c82
Gerardo Barzola <gbarzolas@pucp.pe>
2016-01-23 02:54:42 +0000

    mejora de UX

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  
   root to: "pages#home"
 
   get '/home', to: 'pages#home',as: 'home'
-  get '/about',to: 'pages#about', as: 'about'
-  get '/contact',to: 'pages#contact', as: 'contact'
+  get '/about', to: 'pages#about', as: 'about'
+  get '/contact', to: 'pages#contact', as: 'contact'
   
   devise_for :users
 
